7e065419360ac5f2247ad532ad4be793c5e42ccd
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-09-11 19:09:12 +0700

    fix SearchContent UI

--- app/Search/components/SearchContent.tsx
+++ app/Search/components/SearchContent.tsx
@@ -23,16 +23,19 @@ const SearchContent = ({ songs }: SearchContentProps) => {
     <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
-          <div className="flex-1">
-            <MediaItem
-              data={song}
-              onClick={(id: string) => {
-                onPlay(id);
-              }}
-            />
-          </div>
+        <div className="flex-1 min-w-0">
+          <MediaItem
+            data={song}
+            onClick={(id: string) => {
+              onPlay(id);
+            }}
+          />
+        </div>
+        <div className="shrink-0">
           <LikeButton songId={song.id} />
         </div>
+      </div>
+      
       ))}
     </div>
   );
