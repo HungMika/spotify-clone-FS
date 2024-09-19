55b36426b0abe6783b2d468b23f2c25927654a3b
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-09-19 18:20:09 +0700

    fix LikedUI

--- app/Liked/components/LikedContent.tsx
+++ app/Liked/components/LikedContent.tsx
@@ -42,7 +42,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
     <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
-          <div className="flex-1">
+          <div className="flex-1 sm:max-w-[300px] md:max-w-full min-[300px]:max-w-[270px]">
             <MediaItem
               data={song}
               onClick={(id: string) => {
